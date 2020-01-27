0a8e1f7a6940a9605f103fb716cf4842b2168ec6
Adam Broquet <adambroquet@gmail.com>
2020-01-27 15:56:26 +0100

    Hello TP

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -7,18 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class WelcomeController extends AbstractController
 {   
+  
     /**
-     * @Route("/hello", name="hello")
+     * @Route("/hello/{slug}", name="hello",
+     * requirements={"slug"="\D{3,8}"}
+     * 
+     * )
+     * le requirement va permettre de limiter la saisie a des lettres uniquement avec l'antislash D et le 3,8 limite le nombre de caractères accepetés
      */
-
-    public function hello()
+    public function show($slug = "Adama le roi d'la roumba")
     {
-        $name = "Adama le roi d'la roumba";
-
-        dump($name);
-
         return $this->render('welcome/hello.html.twig', [
-            'name' => $name,
+            'slug' => ucfirst($slug),
         ]);
     }
 }
